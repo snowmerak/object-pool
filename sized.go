eced6717b47a7315f72e9ff409a27416e91f3b7f
snowmerak <snowmerak@outlook.com>
2023-01-14 16:06:10 +0900

    add sized pool

---
+++ sized.go
@@ -0,0 +1,32 @@
+package pool
+
+type SizedPool[T any] struct {
+	pool  chan *T
+	block bool
+}
+
+func NewSized[T any](size int, block bool) *SizedPool[T] {
+	return &SizedPool[T]{
+		pool:  make(chan *T, size),
+		block: block,
+	}
+}
+
+func (p *SizedPool[T]) Get() *T {
+	select {
+	case x := <-p.pool:
+		return x
+	default:
+		if p.block {
+			return nil
+		}
+		return new(T)
+	}
+}
+
+func (p *SizedPool[T]) Put(x *T) {
+	select {
+	case p.pool <- x:
+	default:
+	}
+}
